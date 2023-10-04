ee6018b76deda65fd0039ab0917c2d5d4f447349
alpe@pc <almeidavs@yahoo.com.br>
2023-10-03 21:24:27 -0300

    HTML/CSS/JS intermediário - Formulário com validação

---
+++ 2-formulario-com-validacao/src/js/script.js
@@ -0,0 +1,25 @@
+function validateInput(input, adderror = false){
+    if (input.checkValidity()){
+        input.classList.remove("error");
+        input.classList.add("ok");
+    } else {
+        if (adderror) input.classList.add("error");
+        input.classList.remove("ok");
+    }
+}
+
+function validateForm() {
+    inputs.forEach((input) => {
+         validateInput(input, true);
+    });
+}
+
+const inputs = document.querySelectorAll(".formulario input:not([type=submit]), .formulario textarea");
+
+document.querySelector(".formulario input[type=submit]").addEventListener('click', validateForm);
+
+inputs.forEach(input => {
+    input.addEventListener('input', function(){
+        validateInput(input);
+    });
+});
